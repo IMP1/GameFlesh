2e2a756988875a4891a262e9dbc7c62c4a14b68d
IMP1 <IMP1@mail.com>
2015-05-13 18:20:40 +0100

    moved Main into non-default package

--- src/run/Main.java
+++ src/run/Main.java
@@ -1,3 +1,5 @@
+package run;
+
 import scn.Title;
 
 public class Main extends run.Game {
@@ -8,6 +10,14 @@ public class Main extends run.Game {
 	
 	public static void main(String[] args) {
 		new Main();
+	}
+	
+	public static void puts(Object... args) {
+		for (Object o : args) {
+			System.out.print(o);
+			System.out.print(" ");
+		}
+		System.out.print("\n");
 	}
 
 }
