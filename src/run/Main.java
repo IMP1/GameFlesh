743f0d130bd2c0b6d0e289c67f6bab04ad057aa5
IMP1 <IMP1@mail.com>
2015-05-14 12:10:01 +0100

    added DEBUGGING flag

--- src/run/Main.java
+++ src/run/Main.java
@@ -4,20 +4,14 @@ import scn.Title;
 
 public class Main extends run.Game {
 
+	public static final boolean DEBUGGING = false;
+	
 	public Main() {
-		super(new Title(), "Mnul", 960, 640, 15);
+		super(new Title(), "Depths", 960, 640, 15);
 	}
 	
 	public static void main(String[] args) {
 		new Main();
-	}
-	
-	public static void puts(Object... args) {
-		for (Object o : args) {
-			System.out.print(o);
-			System.out.print(" ");
-		}
-		System.out.print("\n");
 	}
 
 }
