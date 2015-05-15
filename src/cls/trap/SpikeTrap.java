87665599adb8f1ed4b5fca5c29366dca22b9c143
IMP1 <IMP1@mail.com>
2015-05-15 18:37:42 +0100

    made spike and swinging traps more identifiable

--- src/cls/trap/SpikeTrap.java
+++ src/cls/trap/SpikeTrap.java
@@ -10,5 +10,27 @@ public class SpikeTrap extends Trap {
 	public void trigger(scn.Map scene) {
 		
 	}
+	
+	@Override
+	public void draw() {
+		if (((scn.Map)scn.SceneManager.scene()).hasVisited(triggerX, triggerY)) {
+			int w = cls.Level.TILE_SIZE;
+			int x = triggerX * w;
+			int y = triggerY * w;
+			jog.Graphics.setColour(64, 64, 64, 64);
+			jog.Graphics.circle(true, x + 16, y + 8, 3);
+			jog.Graphics.circle(true, x + 32, y + 8, 3);
+			jog.Graphics.circle(true, x +  8, y + 16, 3);
+			jog.Graphics.circle(true, x + 24, y + 16, 3);
+			jog.Graphics.circle(true, x + 40, y + 16, 3);
+			jog.Graphics.circle(true, x + 16, y + 24, 3);
+			jog.Graphics.circle(true, x + 32, y + 24, 3);
+			jog.Graphics.circle(true, x +  8, y + 32, 3);
+			jog.Graphics.circle(true, x + 24, y + 32, 3);
+			jog.Graphics.circle(true, x + 40, y + 32, 3);
+			jog.Graphics.circle(true, x + 16, y + 40, 3);
+			jog.Graphics.circle(true, x + 32, y + 40, 3);
+		}
+	}
 
 }
--- src/cls/trap/SwingingTrap.java
+++ src/cls/trap/SwingingTrap.java
@@ -18,5 +18,20 @@ public class SwingingTrap extends Trap {
 	public void trigger(scn.Map scene) {
 		
 	}
+	
+	@Override
+	public void draw() {
+		if (((scn.Map)scn.SceneManager.scene()).hasVisited(triggerX, triggerY)) {
+			int w = cls.Level.TILE_SIZE;
+			int h = cls.Level.TILE_SIZE;
+			jog.Graphics.setColour(64, 64, 64, 64);
+			if (direction == Direction.HORIZONTAL) {
+				jog.Graphics.rectangle(true, triggerX * w, triggerY * h + 10, w, h - 20);
+			} else {
+				jog.Graphics.rectangle(true, triggerX * w + 10, triggerY * h, w - 20, h);
+			}
+			
+		}
+	}
 
 }
