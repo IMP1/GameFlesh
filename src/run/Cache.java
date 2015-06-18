3213aa1f7451421739bd1966d592de6ac3657669
IMP1 <IMP1@mail.com>
2015-06-18 16:37:49 +0100

    no longer need catching of exception

--- src/run/Cache.java
+++ src/run/Cache.java
@@ -1,6 +1,5 @@
 package run;
 
-import java.io.IOException;
 import java.util.HashMap;
 
 public class Cache {
@@ -11,11 +10,7 @@ public class Cache {
 	public static jog.Image image(String path) {
 		path = "gfx/" + path;
 		if (!images.containsKey(path)) {
-			 try {
-				images.put(path, new jog.Image(path));
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
+			images.put(path, new jog.Image(path));
 		}
 		return images.get(path);
 	}
