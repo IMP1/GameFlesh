9482f9ba8ceeaa39aec2f0088f332c4a5cd07a98
IMP1 <IMP1@mail.com>
2015-07-10 16:43:13 +0100

    added placeholder music

--- src/run/Cache.java
+++ src/run/Cache.java
@@ -6,6 +6,7 @@ public class Cache {
 	private Cache() {}
 	
 	private static HashMap<String, jog.Image> images = new HashMap<String, jog.Image>();
+	private static HashMap<String, jog.Audio.Source> audio = new HashMap<String, jog.Audio.Source>();
 	
 	public static jog.Image image(String path) {
 		path = "gfx/" + path;
@@ -13,6 +14,14 @@ public class Cache {
 			images.put(path, new jog.Image(path));
 		}
 		return images.get(path);
+	}
+	
+	public static jog.Audio.Source audio(String path) {
+		path = "sfx/" + path;
+		if (!audio.containsKey(path)) {
+			audio.put(path, jog.Audio.newSource(path));
+		}
+		return audio.get(path);
 	}
 	
 }
